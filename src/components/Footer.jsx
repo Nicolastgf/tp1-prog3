85bbcfe0a7df3aee7fcdd6b75ee3b388c5b13574
nicolasferdez <nicolasferdeztgf>
2025-04-27 19:25:10 -0300

    ingrese todo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,43 @@
+import { FaInstagram, FaFacebook, FaGithub } from "react-icons/fa";
+import "../css/Footer.css"; 
+
 const Footer = () => {
   return (
-    <footer className="text-center py-3">
-      <p>© 2025 - Todos los derechos reservados</p>
+    <footer className="footer text-center py-3">
+      <div className="footer-icons">
+        {/* Instagram */}
+        <a
+          href="https://www.instagram.com/nicolasferdez/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-xl"
+          aria-label="Instagram"
+        >
+          <FaInstagram />
+        </a>
+
+        {/* Facebook */}
+        <a
+          href="https://www.facebook.com/tuusuario"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-xl fa-facebook"
+          aria-label="Facebook"
+        >
+          <FaFacebook />
+        </a>
+
+        {/* GitHub */}
+        <a
+          href="https://github.com/tuusuario"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-xl fa-github"
+          aria-label="GitHub"
+        >
+          <FaGithub />
+        </a>
+      </div>
     </footer>
   );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,11 @@
+import Yo from "../assets/yo.jpeg"
+import "../css/Main.css"
 const Main = ({ datos }) => {
   return (
     <div className="d-flex justify-content-center">
       <div className="card" style={{ width: "18rem" }}>
         <div className="card-body">
+          <img className="imagen" src={Yo} alt="Yo" />
           <h5 className="card-title">
             {datos.nombre} {datos.apellido}
           </h5>
